eb6f202a67390b466287bf97642c63ed9637b219
amn-96 <60272348+amn-96@users.noreply.github.com>
2025-12-14 21:47:58 -0800

    Added skeleton code for a romm api-based sync code. This one has a fairly bright future. python unit tests are now packaged up in their own ci script to run dynamically
    With the addition of romm_sync, this is probably going to become a monorepo

---
+++ src/romm_sync.py
@@ -0,0 +1,66 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import pandas as pd
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+
+def get_romm_list(romm_url: str, username: str, passwd: str) -> dict:
+    """Gets the full list of ROM's in the ROMM.app database"""
+    
+    url = f"{romm_url}/api/roms/"
+    response = requests.get(url, auth=HTTPBasicAuth(username, passwd))
+
+    logger.debug(f"Status Code: {response.status_code}")
+    data = response.json()
+    logger.debug(f"Type of response: {type(data)}")
+    logger.debug("Response received successfully")
+
+    return data
+
+
+class RetroGameServer:
+    def __init__(self, raw_database, library, by_id, by_platform, games):
+        self.raw_database: dict = raw_database
+        self.library: pd.DataFrame = library
+        self.by_id: dict = by_id
+        self.by_platform: dict = by_platform
+        self.games: list = games
+    
+    @classmethod
+    def initialize_romm_map(cls,
+                            romm_url: str = "https://emu.amnserv.xyz", 
+                            username: str = "akshay", 
+                            passwd: str = "inagalaxyfarfaraway"):
+        """Use this factory method to initialize the sync with a snapshot of the ROMM database"""
+        data = get_romm_list(romm_url, username, passwd)
+
+        # represent the library as a dataframe for ease of use
+        library = pd.json_normalize(data['items'])
+
+        # build a map of ROM ID : ROM NAME
+        id_map = dict(zip(library['id'], library['name']))
+        platform_map = library.groupby('platform_display_name')['name'].apply(list).to_dict()
+        games_list = library['name'].tolist()
+        id_list = library['id'].tolist()
+
+        return cls(raw_database=data, library=library, by_id=id_map, by_platform=platform_map, games=games_list)
+
+    def count(self):
+        return len(self.games)
+    
+    def size_gb(self):
+        return round(self.library['fs_size_bytes'].sum() / 1e9, 2)
+
+
+if __name__ == "__main__":
+    roms = RetroGameServer.initialize_romm_map()
+
+    print(roms.count())
+    print(roms.size_gb())
+
